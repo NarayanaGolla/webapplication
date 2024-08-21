1a154bbc69c99470810fc2086a4bd64e9ebf4bce
NarayanaGolla <narayana.golla.aws@gmail.com>
2024-08-21 19:15:55 +0530

    reactjs files

--- spring-web/src/test/java/com/web/application/controller/RegistrationControllerTest.java
+++ spring-web/src/test/java/com/web/application/controller/RegistrationControllerTest.java
@@ -1,30 +1,30 @@
-package com.web.application.controller;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-public class RegistrationControllerTest extends AbstractTest  {
-
-
-
-    @BeforeEach
-    public void setup() {
-        System.out.println("hello");
-
-    }
-
-    @Test
-    public void testRegisterUser_ValidUser()  throws Exception {
-
-
-    }
-}
+//package com.web.application.controller;
+//
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//@AutoConfigureMockMvc
+//public class RegistrationControllerTest extends AbstractTest  {
+//
+//
+//
+//    @BeforeEach
+//    public void setup() {
+//        System.out.println("hello");
+//
+//    }
+//
+//    @Test
+//    public void testRegisterUser_ValidUser()  throws Exception {
+//
+//
+//    }
+//}
